b9bd4860ecb007ebf33ea00b49c1fa4859078937
bhagyasah <bhagyasah4u@gmail.com>
2019-05-23 11:27:28 +0545

    geting audio and video in react is easy so its done

---
+++ react-webRtc/web-rtc/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Screen from './screen';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Screen />
+    </div>
+  );
+}
+
+export default App;
