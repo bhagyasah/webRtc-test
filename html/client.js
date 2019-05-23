29f9ec89976335dc0e54e85c13399891bb6f0242
bhagyasah <bhagyasah4u@gmail.com>
2019-05-23 09:56:35 +0545

    react and node server repo added

--- html/client.js
+++ html/client.js
@@ -1,12 +1,12 @@
-// function hasUserMedia() {
-//   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
-//      || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-//   return navigator.getUserMedia;
-// }
-// console.log(hasUserMedia());
-// if (true) {
-  // navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
-  //    || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+function hasUserMedia() {
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
+     || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+  return navigator.getUserMedia;
+}
+console.log(hasUserMedia());
+if (true) {
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
+     || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
   //get both video and audio streams from user's camera
   navigator.getUserMedia({ video: true, audio: false }, function (stream) {
@@ -14,12 +14,13 @@
 
      //insert stream into the video tag
     //  video.src = window.URL.createObjectURL(stream);
+      console.log(stream);
      video.srcObject = stream;
   }, function (err) {});
 
   let peerConn = new RTCPeerConnection();
   let dataCannel = peerConn.createDataChannel('myChanel');
   console.log(dataCannel);
-// } else {
-//   alert("Error. WebRTC is not supported!");
-// }
+} else {
+  alert("Error. WebRTC is not supported!");
+}
--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const http = require('http');
 const run = require('app-node');
 const app = express();
 const path = require('path');
+const webSocket = require('ws');
 
 const server = http.createServer(app);
   app.use(express.static(path.resolve(__dirname,'.','html')));
-  server.listen(6001);
-  console.log(`server started at port 6001`);
+  server.listen(9090);
+  console.log(`server started at port 9090`);
+
+  const wss =  new webSocket.Server({port:9091});
+
+  wss.on('connection', (ws, req) => {
+    console.log('onconection called');
+    wss.onmessage((msg) => {
+      console.log(msg);
+    })
+  })
